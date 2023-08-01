922eff8ca49f7893b54da45cdf34d4316cc52ef4
onurokkyay <onurokyay1@gmail.com>
2023-08-01 18:12:01 +0300

    MovieCard body title

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,24 +4,13 @@ import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }) => {
   return (
-    <div className="col" style={{ width: "300px", height: "1200px" }}>
+    <div className="col" style={{ width: "300px", height: "480px" }}>
       <Card style={{ width: "100%", height: "100%" }}>
         <Link to={`../movies/${movie.id}`}>
           <Card.Img variant="top" src={movie.posterPath} alt={movie.title} />
         </Link>
-        <Card.Body>
+        <Card.Body style={{ maxHeight: "160px", overflow: "hidden" }}>
           <Card.Title>{movie.title}</Card.Title>
-          <Card.Text>Release Date: {movie.releaseDate}</Card.Text>
-          <Card.Text>Original Language: {movie.originalLanguage}</Card.Text>
-          <Card.Text>Original Title: {movie.originalTitle}</Card.Text>
-          <Card.Text>Popularity: {movie.popularity}</Card.Text>
-          <Card.Text>Vote Average: {movie.voteAverage}</Card.Text>
-          <Card.Text>Vote Count: {movie.voteCount}</Card.Text>
-          <Card.Text>Overview: {movie.overview}</Card.Text>
-          <div style={{ marginTop: "auto", paddingTop: "10px" }}>
-            <Button variant="primary">Button 1</Button>
-            <Button variant="secondary">Button 2</Button>
-          </div>
         </Card.Body>
       </Card>
     </div>
